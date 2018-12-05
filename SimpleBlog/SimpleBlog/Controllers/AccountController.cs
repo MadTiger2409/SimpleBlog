d789c1029b3f82e329b062b582ffc8fa5c83d445
Michał B <cyberwolf_96@outlook.com>
2018-12-05 18:53:26 +0100

    Working on registration

--- SimpleBlog/SimpleBlog/Controllers/AccountController.cs
+++ SimpleBlog/SimpleBlog/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SimpleBlog.Commands.User;
+using SimpleBlog.Extensions;
 
 namespace SimpleBlog.Controllers
 {
@@ -19,6 +21,15 @@ namespace SimpleBlog.Controllers
         [HttpPost("register")]
         public IActionResult Registration(CreateUserCommand command)
         {
+            Regex loginRegex = new Regex(AccountRegex.Login);
+            Regex passwordRegex = new Regex(AccountRegex.Password);
+
+            if (!loginRegex.Match(command.Login).Success || !passwordRegex.Match(command.Password).Success)
+            {
+                // Return view with error message
+                return View();
+            }
+
             return View();
         }
     }
---
+++ SimpleBlog/SimpleBlog/Extensions/AccountRegex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBlog.Extensions
+{
+    public static class AccountRegex
+    {
+        public static string Login { get; } = @"[a-zA-Z][a-zA-Z0-9]{5,20}";
+        public static string Password { get; } = @"(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,30}";
+    }
+}
