f5b6e4fe1e74a994d3237517419f59a6a380432d
Michał B <cyberwolf_96@outlook.com>
2018-12-22 17:49:34 +0100

    Added data seeder. Need To move it to the 2.1 seeding style.

---
+++ SimpleBlog/SimpleBlog/Data/Database/DataSeeder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SimpleBlog.Data.Models;
+using SimpleBlog.Extensions;
+
+namespace SimpleBlog.Data.Database
+{
+    public static class DataSeeder
+    {
+        public static void Initialize(BlogContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (!context.Admins.Any())
+            {
+                var password = "Adm1n.P@ss";
+                byte[] passwordHash, passwordSalt;
+                var passwordManager = new PasswordManager();
+
+                passwordManager.CalculatePasswordHash(password, out passwordHash, out passwordSalt);
+                context.Admins.Add(new Admin(passwordHash, passwordSalt, "SuperAdmin666"));
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- SimpleBlog/SimpleBlog/Program.cs
+++ SimpleBlog/SimpleBlog/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SimpleBlog.Data.Database;
 
 namespace SimpleBlog
 {
@@ -18,7 +20,26 @@ namespace SimpleBlog
             //CreateWebHostBuilder(args).UseSetting("https_port","443")
             //    .Build().Run();
 
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetService<BlogContext>();
+
+                try
+                {
+                    DataSeeder.Initialize(context);
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
